ed5f6c81e14fe43be35ceaf88c41289c1001232b
saim625 <siamsaeed625@gmail.com>
2025-02-27 15:51:40 +0500

    redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,21 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("saim@gmail.com");
-  const [password, setPassword] = useState("Saim@123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleLogin = async () => {
     try {
-      await axios.post(
-        "http://localhost:3000/login",
+      const res = await axios.post(
+       BASE_URL + "/login",
         {
           emailId,
           password,
@@ -17,6 +24,8 @@ const Login = () => {
           withCredentials: true,
         }
       );
+      dispatch(addUser(res.data))
+      navigate('/')
     } catch (err) {
       console.log(err);
     }
