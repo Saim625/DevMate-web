e1ad54f81029b3780e314cfaed40494088e651af
saim625 <saimsaeed625@gmail.com>
2025-06-14 21:37:42 +0400

    Improve userCard Ui

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,16 +4,16 @@ import { useDispatch } from "react-redux";
 import { removeUserFromFeed } from "../utils/feedSlice";
 import { BASE_URL } from "../utils/constants";
 
-
 const UserCard = ({ user, showButton }) => {
-  const { _id, firstName, lastName, age, gender, skills, imageURL, about } =  user;
-   
+  const { _id, firstName, lastName, age, gender, skills, imageURL, about } =
+    user;
+
   const dispatch = useDispatch();
 
   const handleSendRequest = async (status, userId) => {
     try {
       await axios.post(
-       BASE_URL + "/request/send/" + status + "/" + userId,
+        BASE_URL + "/request/send/" + status + "/" + userId,
         {},
         { withCredentials: true }
       );
@@ -24,39 +24,63 @@ const UserCard = ({ user, showButton }) => {
   };
 
   return (
-      <div className="card bg-base-300 h-auto sm:h-[85vh] w-80 sm:w-96 shadow-lg border border-base-300">
-        <figure>
-          <img
-            src={imageURL}
-            alt="image"
-            className="w-full max-h-64 object-cover"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{firstName + " " + lastName}</h2>
-          {gender && <p>{gender}</p>}
-          {age && <p>{"age: " + age}</p>}
-          <p className="line-clamp-2">{about}</p>
-          {skills && <p>{skills}</p>}
-          {showButton && (
-            <div className="card-actions justify-center">
-              <button
-                className="btn btn-primary"
-                onClick={() => handleSendRequest("ignored", _id)}
-              >
-                Ignore
-              </button>
-              <button
-                className="btn btn-secondary"
-                onClick={() => handleSendRequest("interested", _id)}
-              >
-                Interested
-              </button>
-            </div>
+    <div className="card bg-base-100 w-80 h-full sm:h-[60vh] sm:w-96 shadow-xl border border-base-300 overflow-hidden flex justify-center">
+      <figure className="overflow-hidden">
+        <img
+          src={imageURL}
+          alt="image"
+          className="w-full max-h-64 object-cover"
+        />
+      </figure>
+
+      <div className="card-body flex flex-col justify-start gap-2">
+        <h2 className="card-title text-lg sm:text-xl font-semibold">
+          {firstName + " " + lastName}
+        </h2>
+
+        <div className="text-sm text-base-content/70 space-y-1">
+          {gender && (
+            <p>
+              <span className="font-medium text-base-content">Gender:</span>{" "}
+              {gender}
+            </p>
+          )}
+          {age && (
+            <p>
+              <span className="font-medium text-base-content">Age:</span> {age}
+            </p>
+          )}
+          {skills && (
+            <p>
+              <span className="font-medium text-base-content">Skills:</span>{" "}
+              {skills}
+            </p>
           )}
         </div>
+
+        <p className="text-sm text-base-content line-clamp-2 overflow-hidden">
+          {about}
+        </p>
+
+        {showButton && (
+          <div className="card-actions mt-2 flex justify-center gap-x-4">
+            <button
+              className="btn btn-sm btn-outline btn-error"
+              onClick={() => handleSendRequest("ignored", _id)}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={() => handleSendRequest("interested", _id)}
+            >
+              Interested
+            </button>
+          </div>
+        )}
       </div>
-    )
+    </div>
+  );
 };
 
 export default UserCard;
